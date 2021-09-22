6f460d255d3367ba6eeaed43be6edb529e1316a2
RukhsarAhmed <rukhsar101095@gmail.com>
2021-09-22 19:41:24 +0530

    firstcommit

--- and.py
+++ and.py
@@ -1,26 +1,35 @@
+
+
 from utils.model import Perceptron
 from utils.all_utils import prepare_data, save_plot , save_model
 import pandas as pd
 import numpy as np
 
+def main(data, eta, epochs, filename , PlotFileName):
+    
 
-AND = {
-    "x1": [0,0,1,1],
-    "x2": [0,1,0,1],
-    "y": [0,0,0,1],
-}
+    df = pd.DataFrame(data)
+    print(df)
+    X,y = prepare_data(df)
+    
+    model = Perceptron(eta=eta, epochs=epochs)
+    model.fit(X, y)
 
-df = pd.DataFrame(AND)
+    _ = model.total_loss()
 
-df
+    save_model(model , filename= filename)
+    save_plot(df, PlotFileName, model)
 
-
-X,y = prepare_data(df)
-
-ETA = 0.3 # 0 and 1
-EPOCHS = 10
-
-model = Perceptron(eta=ETA, epochs=EPOCHS)
-model.fit(X, y)
-
-_ = model.total_loss()
+if __name__== '__main__': # This Is The  Entry Point 
+   
+    AND = {
+        "x1": [0,0,1,1],
+        "x2": [0,1,0,1],
+        "y": [0,0,0,1],
+    }
+   
+    
+    ETA = 0.3 # 0 and 1
+    EPOCHS = 10
+   
+    main(data=AND , eta=ETA , epochs=EPOCHS , filename='and.model' , PlotFileName='and.png')
--- utils/all_utils.py
+++ utils/all_utils.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 import joblib # FOR SAVING MY MODEL AS A BINARY FILE
 from matplotlib.colors import ListedColormap
+import logging
 
 
 
@@ -13,6 +14,15 @@ plt.style.use("fivethirtyeight")
 
 
 def prepare_data(df):
+  """ It is used to seperate the dependent and Independent Features
+
+  Args:
+      df (pd.DataFrame): Its the Pandas DataFrame
+
+  Returns:
+      tuple: It is used to return the dependent and Independent Variables
+  """
+  logging.info("prepraing the data by segragating the dependent and independent varable")
   X = df.drop("y", axis=1)
 
   y = df["y"]
@@ -21,17 +31,32 @@ def prepare_data(df):
 
 
 def save_model(model, filename):
+  """This method used to save the Trained Model
+
+  Args:
+      model (python object): trained model to
+      filename (str): path to  save the trained model
+  """
+  logging.info("saving the trained model")
   model_dir = "models"
   os.makedirs(model_dir, exist_ok=True) # ONLY CREATE IF MODEL_DIR DOESN"T EXISTS
   filePath = os.path.join(model_dir, filename) # model/filename
   joblib.dump(model, filePath)
+  logging.info(f"saved the trained model {filePath}")
 
 
-  
-  
   
 def save_plot(df, file_name, model):
+  """param df: Its a dataframe
+     param file_name: its path to save the plot
+     param model: trained model
+
+  
+  """
+
   def _create_base_plot(df):
+    
+    logging.info("creating the base plot")
     df.plot(kind="scatter", x="x1", y="x2", c="y", s=100, cmap="winter")
     plt.axhline(y=0, color="black", linestyle="--", linewidth=1)
     plt.axvline(x=0, color="black", linestyle="--", linewidth=1)
@@ -39,6 +64,7 @@ def save_plot(df, file_name, model):
     figure.set_size_inches(10, 8)
 
   def _plot_decision_regions(X, y, classfier, resolution=0.02):
+    logging.info("plotting the decision regions")
     colors = ("red", "blue", "lightgreen", "gray", "cyan")
     cmap = ListedColormap(colors[: len(np.unique(y))])
 
@@ -50,16 +76,21 @@ def save_plot(df, file_name, model):
 
     xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution), 
                            np.arange(x2_min, x2_max, resolution))
-    print(xx1)
-    print(xx1.ravel())
+   
     Z = classfier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
     Z = Z.reshape(xx1.shape)
     plt.contourf(xx1, xx2, Z, alpha=0.2, cmap=cmap)
     plt.xlim(xx1.min(), xx1.max())
     plt.ylim(xx2.min(), xx2.max())
-
     plt.plot()
-
-
-
+  
   X, y = prepare_data(df)
+
+  _create_base_plot(df)
+  _plot_decision_regions(X, y, model)
+
+  plot_dir = "plots"
+  os.makedirs(plot_dir, exist_ok=True) # ONLY CREATE IF MODEL_DIR DOESN"T EXISTS
+  plotPath = os.path.join(plot_dir, file_name) # model/filename
+  plt.savefig(plotPath)
+  logging.info("saved the plot {plotPath}")
